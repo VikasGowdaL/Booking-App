cc5e7b51f0c2443cb4a7c30b33ed496d9ad30980
VikasGowdaL <vikasgowda6376@gmail.com>
2026-05-23 19:20:01 +0530

    Add files via upload

---
+++ client/src/utils/api.ts
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: import.meta.env.DEV ? 'http://localhost:4500' : 'https://booking-com-api.onrender.com',
+})
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('accessToken')
+  config.headers = {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${token}`,
+  }
+
+  return config
+})
+
+export default api
